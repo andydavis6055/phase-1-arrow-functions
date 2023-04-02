a2407e87f15d847ba2f889bcaab80613f77371ed
Andy Davis <andydavis6055@gmail.com>
2023-04-02 10:17:51 -0600

    tests passing

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(num1, num2){
+    return num1 / num2
+}
+const num1 = 2000
+const num2 = 100
+
+const square = function(Number) {
+    return Number * Number
+}
+const add = function(num3, num4) {
+    return num3 + num4
+}
